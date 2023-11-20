bcf734546c9e362abe89e6a3dc32cf7fdc04f7d8
Mahmudur Rahman <47264267+Mahmud686@users.noreply.github.com>
2023-11-20 01:47:42 +0100

    Update Station.java

--- ChargingStation/src/Station.java
+++ ChargingStation/src/Station.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
-
+import java.util.InputMismatchException;
 import java.util.List;
-
+import java.util.Scanner;
 
 public class Station {
-
 
     private List<Location> Locations;
     private SourceofEnergy SourceofEnergy;
@@ -15,24 +14,54 @@ public class Station {
         for (int i = 0; i < numLocations; i++) {
             Locations.add(new Location());
         }
-
     }
 
-    public boolean chargeCar() {
+    public boolean chargeCar() throws ChargingException {
+        boolean charged = false;
         for (Location location : Locations) {
             if (!location.isOccupied()) {
                 location.occupy();
                 double energyProvided = SourceofEnergy.provideEnergy();
                 System.out.println("Charging the car with " + energyProvided + " kWh of energy");
                 location.release();
-                return true; 
+                charged = true;
+                break;
             }
         }
-        return false; 
+        if (!charged) {
+            throw new ChargingException("All places occupied, unable to charge");
+        }
+        return charged;
     }
 
-    
-    public SourceofEnergy getSourceofEnergy() {
-        return SourceofEnergy;
+    public void displayStationStatus(Scanner scanner, int stationNumber) throws ChargingException {
+        for (int i = 0; i < Locations.size(); i++) {
+            int status = -1;
+            while (status < 0 || status > 1) {
+                System.out.print("Enter status for charging location " + (i + 1) + " at station " + stationNumber + " (1 for Occupied, 0 for Empty): ");
+                try {
+                    status = scanner.nextInt();
+                    if (status < 0 || status > 1) {
+                        System.out.println("Invalid input. Please enter 0 for Empty or 1 for Occupied.");
+                        // Clear the scanner buffer to prevent it from reading the invalid input again
+                        scanner.nextLine();
+                    } else {
+                        if (status == 1) {
+                            Locations.get(i).occupy();
+                        }
+                        String displayStatus = (status == 1) ? "Occupied" : "Empty";
+                        System.out.println("Charging location " + (i + 1) + " at station " + stationNumber + " is: " + displayStatus);
+                    }
+                } catch (InputMismatchException e) {
+                    throw new ChargingException("Error while taking input for charging location status", e);
+                } catch (Exception e) {
+                    throw new ChargingException("Unknown error occurred while taking input for charging location status", e.getCause());
+                } finally {
+                	scanner.close();
+                }
+            }
+        }
     }
+
+
 }
