e66ffb1be385169fb0c5b49d89ec053516be4124
Mahmudur Rahman <47264267+Mahmud686@users.noreply.github.com>
2023-11-20 20:45:22 +0100

    Update Main.java

--- ChargingStation/src/Main.java
+++ ChargingStation/src/Main.java
@@ -1,90 +1,68 @@
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Station {
 
-    public static void main(String[] args) {
+    private List<Location> Locations;
+    private SourceofEnergy SourceofEnergy;
 
-        Scanner scanner = new Scanner(System.in);
+    public Station(int numLocations, SourceofEnergy SourceofEnergy) {
+        this.SourceofEnergy = SourceofEnergy;
+        this.Locations = new ArrayList<>(numLocations);
+        for (int i = 0; i < numLocations; i++) {
+            Locations.add(new Location());
+        }
+    }
 
-        int numStations = -1;
-        int numLocations = -1;
-
-        while (numStations < 1) {
-            System.out.print("Enter the number of charging stations: ");
-            try {
-                numStations = scanner.nextInt();
-                if (numStations < 1) {
-                    throw new IllegalArgumentException("Exception: Must be a positive integer");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Exception: Must be an integer");
-                scanner.next();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e);
-                //throw e;
+    public boolean chargeCar() throws ChargingException {
+        boolean charged = false;
+        for (Location location : Locations) {
+            if (!location.isOccupied()) {
+                location.occupy();
+                double energyProvided = SourceofEnergy.provideEnergy();
+                System.out.println("Charging the car with " + energyProvided + " kWh of energy");
+                location.release();
+                charged = true;
+                break;
             }
         }
-
-        while (numLocations < 1) {
-            System.out.print("Enter the number of charging locations at each station: ");
-            try {
-                numLocations = scanner.nextInt();
-                if (numLocations < 1) {
-                    throw new IllegalArgumentException("Exception: Must be a positive integer");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Exception: Must be an integer");
-                scanner.next();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e);
-              //throw e;
-            }
+        if (!charged) {
+            throw new ChargingException("All places occupied, unable to charge");
         }
+        return charged;
+    }
 
-        Station[] chargingStations = new Station[numStations];
-
-        for (int i = 0; i < numStations; i++) {
-            int weatherChoice = -1;
-
-            while (weatherChoice < 0 || weatherChoice > 2) {
-                System.out.print("Enter weather condition for station " + (i + 1) + " (0 for SUNNY, 1 for WINDY, 2 for RAINY): ");
+    public void displayStationStatus(Scanner scanner, int stationNumber) throws ChargingException {
+        for (int i = 0; i < Locations.size(); i++) {
+            int status = -1;
+            while (status < 0 || status > 1) {
+                System.out.print("Enter status for charging location " + (i + 1) + " at station " + stationNumber + " (1 for Occupied, 0 for Empty): ");
                 try {
-                    weatherChoice = scanner.nextInt();
-                    if (weatherChoice < 0 || weatherChoice > 2) {
-                        throw new IllegalArgumentException("Invalid weather condition");
+                    status = scanner.nextInt();
+                    if (status < 0 || status > 1) {
+                        System.out.println("Invalid input. Please enter 0 for Empty or 1 for Occupied.");
+                        // Clear the scanner buffer to prevent it from reading the invalid input again
+                        scanner.nextLine();
+                    } else {
+                        if (status == 1) {
+                            Locations.get(i).occupy();
+                        }
+                        String displayStatus = (status == 1) ? "Occupied" : "Empty";
+                        System.out.println("Charging location " + (i + 1) + " at station " + stationNumber + " is: " + displayStatus);
                     }
                 } catch (InputMismatchException e) {
-                    System.out.println("Exception: Must be an integer");
-                    scanner.next();
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e);
-                  //throw e;
+                    throw new ChargingException("Error while taking input for charging location status", e);
+                } catch (Exception e) {
+                    throw new ChargingException("Unknown error occurred while taking input for charging location status", e.getCause());
                 }//finally{
-            	//scanner.close();
-            //}
-            }
-
-            try {
-                if (weatherChoice == 0) {
-                    System.out.println("Switching to Solarpanel energy source.");
-                    chargingStations[i] = new Station(numLocations, new SolarPanel());
-                } else if (weatherChoice == 1) {
-                    System.out.println("Switching to WindTurbine energy source.");
-                    chargingStations[i] = new Station(numLocations, new WindTurbine());
-                } else if (weatherChoice == 2) {
-                    System.out.println("Switching to electricity energy source.");
-                    chargingStations[i] = new Station(numLocations, new Electricity());
-                }
-
-                if (chargingStations[i] != null) {
-                    chargingStations[i].displayStationStatus(scanner, weatherChoice);
-                }
-            } catch (Exception e) {
-                System.out.println("Exception occurred during station creation: " + e.getMessage());
-              //throw e;
+                	//scanner.close();
+                //}
             }
         }
+    }
+}
 
         scanner.close();
     }
